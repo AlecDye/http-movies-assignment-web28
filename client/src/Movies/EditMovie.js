6afb1bd57dc2b159cb99534c110618921ce8ed35
Alec Dye <alecdye@gmail.com>
2020-03-25 14:15:13 -0700

    Building edit movie component

--- client/src/Movies/EditMovie.js
+++ client/src/Movies/EditMovie.js
@@ -10,16 +10,76 @@ const initialMovie = {
   stars: []
 };
 
-const EditMovie = () => {
+const EditMovie = props => {
   const match = useRouteMatch();
   const history = useHistory();
   const [movie, setMovie] = useState(initialMovie);
 
+  const handleChanges = e => {
+    e.persist();
+    setMovie({
+      ...movie,
+      [e.target.name]: e.target.value
+    });
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.put(`http://localhost:5000/movies/${movie.id}`, movie).then(res => {
+      //   console.log(".put", res);
+      const editedMovies = props.movies.map(item => {
+        if (item.id === res.data.id) {
+          return res.data;
+        } else {
+          return item;
+        }
+      });
+      props.setMovie(editedMovies);
+      history.push("/");
+    });
+  };
+
+  useEffect(() => {
+    const movie = props.movies.find(item => `${item.id}` === match.params.id);
+    if (movie) {
+      setMovie(movie);
+    }
+  }, [props.movie, match]);
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Edit Movie</h2>
-      <input />
-      <button>Edit Movie</button>
+      <label for="title">Title:</label>
+      <input
+        id="title"
+        type="text"
+        name="title"
+        value={movie.title}
+        onChange={handleChanges}
+      />
+      <label for="director">Director:</label>
+      <input
+        id="director"
+        type="text"
+        name="director"
+        value={movie.director}
+        onChange={handleChanges}
+      />
+      <label for="metascore">Metascore:</label>
+      <input
+        id="metascore"
+        type="tel"
+        name="metascore"
+        value={movie.metascore}
+        onChange={handleChanges}
+      />
+      <label for="stars">Stars:</label>
+      <textarea
+        id="stars"
+        type="text"
+        name="stars"
+        value={movie.stars}
+        onChange={handleChanges}
+      />
+      <button>Save</button>
     </form>
   );
 };
